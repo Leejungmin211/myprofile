c58ff7a3a353dafb714e8c0b3bae8a67de720bbf
Leejungmin211 <jungmin910211@gmail.com>
2023-06-21 22:56:01 +0900

    feat: layout 설정

--- homepage/src/app/page.tsx
+++ homepage/src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import Career from '@/components/main/Career';
+import MyProfile from '@/components/main/MyProfile';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">이력서</div>
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <MyProfile />
+        <Career />
+      </div>
     </main>
   );
 }
