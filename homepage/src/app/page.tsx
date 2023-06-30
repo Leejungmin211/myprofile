00abfcd128c5aca5ad4e0cbdde147cc85fb52d49
Leejungmin211 <jungmin910211@gmail.com>
2023-06-30 23:28:28 +0900

    feat: project 컴포넌트 추가

--- homepage/src/app/page.tsx
+++ homepage/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Career from '@/components/main/Career';
 import MyProfile from '@/components/main/MyProfile';
+import Project from '@/components/main/Project';
 import Image from 'next/image';
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <MyProfile />
         <Career />
+        <Project />
       </div>
     </main>
   );
---
+++ homepage/src/components/main/Project.tsx
@@ -0,0 +1,8 @@
+export default function Project() {
+    return(
+        <div>
+            <h1>기술스택</h1>
+            <h1>포토폴리오</h1>
+        </div>
+    )
+}
