32ff1757ea7d1f37da2e861c3ae2cf60e47d20c0
Leejungmin211 <jungmin910211@gmail.com>
2023-07-04 23:48:25 +0900

    feat: 기술스택, 포토폴리오 파일 추가

--- homepage/src/components/main/Project.tsx
+++ homepage/src/components/main/Project.tsx
@@ -1,8 +1,13 @@
+import Portfolio from '../project/Portfolio';
+import TeckStack from '../project/TeckStack';
+
 export default function Project() {
-    return(
-        <div>
-            <h1>기술스택</h1>
-            <h1>포토폴리오</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>기술스택</h1>
+      <TeckStack />
+      <h1>포토폴리오</h1>
+      <Portfolio />
+    </div>
+  );
 }
---
+++ homepage/src/components/project/TeckStack.tsx
@@ -0,0 +1,10 @@
+export default function TeckStack() {
+  return (
+    <div>
+      <div>
+        <h2>React</h2>
+        <p>컴포넌트 재사용과 ...</p>
+      </div>
+    </div>
+  );
+}
