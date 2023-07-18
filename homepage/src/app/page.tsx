e1464c03f41ede778074d28957ba416cc42d507e
Leejungmin211 <jungmin910211@gmail.com>
2023-07-18 22:48:37 +0900

    feat: data 파일 추가

--- homepage/src/app/page.tsx
+++ homepage/src/app/page.tsx
@@ -1,16 +1,16 @@
+import React from 'react';
 import Career from '@/components/main/Career';
 import MyProfile from '@/components/main/MyProfile';
 import Project from '@/components/main/Project';
+import ReactFullpage from '@fullpage/react-fullpage';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <MyProfile />
-        <Career />
-        <Project />
-      </div>
+      <MyProfile />
+      <Career />
+      <Project />
     </main>
   );
 }
