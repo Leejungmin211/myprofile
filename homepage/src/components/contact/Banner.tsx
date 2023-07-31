c56e115e7df3154acc0bb5b12445ce3ebc5602c6
Leejungmin211 <jungmin910211@gmail.com>
2023-07-31 19:34:32 +0900

    layout: banner 레이아웃 수정

--- homepage/src/components/contact/Banner.tsx
+++ homepage/src/components/contact/Banner.tsx
@@ -11,6 +11,6 @@ export default function Banner({
   const isSuccess = state === 'success';
   const icon = isSuccess ? '✔️' : '❌';
   return (
-    <p className="mb-6 py-2 rounded-md text-center bg-yellow_green text-slate-800">{`${icon} ${message}`}</p>
+    <p className="mb-6 py-2 rounded-md text-center bg-yellow_green text-slate-800 xsm:mb-3 xsm:text-sm">{`${icon} ${message}`}</p>
   );
 }
