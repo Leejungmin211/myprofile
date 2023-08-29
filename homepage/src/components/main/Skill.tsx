651b9391ce314f4500b3021303b04bf7f8da8c6e
Leejungmin211 <jungmin910211@gmail.com>
2023-08-29 16:48:14 +0900

    feat: main 브랜치 내용 추가 업데이트

--- homepage/src/components/main/Skill.tsx
+++ homepage/src/components/main/Skill.tsx
@@ -2,6 +2,7 @@ import TeckStack from '../skill/TeckStack';
 
 export default function Skill() {
   const stackData = require('/data/teckStack.json');
+  
   return (
     <section className="flex flex-col justify-center items-center mt-10 relative xsm:mt-0">
       <h1 className="absolute top-1 left-5 xlg:text-5xl md:text-4xl sm:text-2xl xsm:text-2xl font-bold text-pink">
